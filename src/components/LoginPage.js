614855a8b60562020cebfaa50bffe1ac63a2f501
janecyyu <janecyyu@gmail.com>
2020-03-30 11:24:15 -0700

    add login page form

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {withFormik, Form, Field} from 'formik';
+import {Button} from "reactstrap";
 
 const LoginPage = () => {
     return (
-        <div>
-            LoginPage Component
-        </div>
+        <Form>
+            <h5>Login</h5>
+            <Field name="email" type="email" placeholder="Email"></Field><br/>
+            <Field name="password" type="password" placeholder="Password"></Field><br/>
+            <Button color="info" size="sm">Submit</Button>
+        </Form>
     )
 }
 
-export default LoginPage
+const FormikLoginPage = withFormik({})(LoginPage)
+
+export default FormikLoginPage
