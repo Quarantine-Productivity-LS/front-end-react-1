f44fca0f5f6443df30d016838a98ea21d9b1abeb
janecyyu <janecyyu@gmail.com>
2020-03-31 16:25:56 -0700

    fix some style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { PrivateRoute } from './utils/PrivateRoute'
 import Header from './components/Header'
 import TaskList from './components/TaskList'
 import FormikLoginPage from './components/LoginPage'
 import FormikRegisterPage from './components/Register'
 import './App.css';
-import Header from './components/Header'
 
 const App = () => {
   return (
