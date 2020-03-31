b0d493b23fe436eb2863169fbd5e521ba2205a19
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-03-31 12:31:56 -0600

    changed styling convention for task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Moment from 'react-moment'
 import { toggleCompletion } from '../actions/taskActions'
 import { connect } from 'react-redux'
 import './Task.css'
 
 const Task = props => {
+    const [expanded, setExpanded] = useState(false);
     return (
-        <div className={props.task.completed ? "task-container finished" : "task-container"}>
-            <div className="task">
-                <div className={props.task.completed ? "checkbox completed" : "checkbox"} onClick={() => props.toggleCompletion(props.tasks, props.task.id)}>
-                <div className={props.task.completed ? "checkmark" : "checkmark hidden"}>{props.task.completed && <i className="fas fa-check"></i>}</div>
+        <div className={expanded ? "task-container expanded" : "task-container"} onClick={() => setExpanded(!expanded)}>
+            <div className={props.task.completed ? "shown-info finished" : "shown-info"}>
+                <div className="task">
+                    <div className={props.task.completed ? "checkbox completed" : "checkbox"} onClick={() => props.toggleCompletion(props.tasks, props.task.id)}>
+                    <div className={props.task.completed ? "checkmark" : "checkmark hidden"}>{props.task.completed && <i className="fas fa-check"></i>}</div>
+                    </div>
+                    <div>{props.task.name}</div>
                 </div>
-                <div>{props.task.name}</div>
+                <div className="tags" style={(props.task.tags[0].length > 0) ? {display:"flex"} : {display:"none"}}>
+                    {props.task.tags.map((tag, index) => <div key={`task-${props.task.id}-tag-${index}`} className={(props.activeTab === tag) ? "tag active-tag" : "tag"} onClick={() => props.toggleTag(tag)}>{tag}</div>)}
+                </div>
+                <div className="date"><Moment format="MMM Do, YYYY" date={props.task.due}/></div>
             </div>
-            <div className="tags" style={(props.task.tags[0].length > 0) ? {display:"flex"} : {display:"none"}}>
-                {props.task.tags.map((tag, index) => <div key={`task-${props.task.id}-tag-${index}`} className={(props.activeTab === tag) ? "tag active-tag" : "tag"} onClick={() => props.toggleTag(tag)}>{tag}</div>)}
-            </div>
-            <div className="date"><Moment format="MMM Do, YYYY" date={props.task.due}/></div>
         </div>
     )
 }
