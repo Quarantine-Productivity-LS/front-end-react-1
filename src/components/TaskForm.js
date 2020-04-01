dcc16546e78472a2c4bc31f2a0f0c4445159de0c
Alexander Karren <alexanderkarren@Alexanders-MacBook-Pro.local>
2020-04-01 10:27:37 -0600

    updated styles on tasks to reflect login and register

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -86,7 +86,7 @@ const TaskForm = props => {
                         <label htmlFor="description">Notes</label>
                         <textarea type="text" id="description" name="description" onChange={handleChanges} value={values.description}/>
                     </div>
-                    <div onClick={e => e.stopPropagation()}>{props.isPosting ? <Spinner color="primary"/> : <Button type="submit" color="primary">Add Task</Button>}</div>
+                    <div onClick={e => e.stopPropagation()}>{props.isPosting ? <Spinner color="primary"/> : <Button type="submit" >Add Task</Button>}</div>
                     <div>{(props.error.length > 0) && props.error}</div>
                 </div>
             </form>
